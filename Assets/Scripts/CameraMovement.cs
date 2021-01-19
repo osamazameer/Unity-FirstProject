56a0c11ae08014c3f24c0618c7cd5ffc1ebb5429
osamazamir <ocamahere@gmail.com>
2021-01-19 19:54:33 +0500

    3rd Person Player Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform Target, Player;
+    public float rotationSpeed;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+            mouseX += Input.GetAxis("Mouse X");
+            mouseY -= Input.GetAxis("Mouse Y");
+           // mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+            transform.LookAt(Target);
+
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float movementSpeed;  
+    
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        
+        Vector3 playerMovement = new Vector3(xAxis, 0f, zAxis) * movementSpeed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+    }
+}
